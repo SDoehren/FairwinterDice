25cebb3717187f93096912113ace14826b3b0107
SDoehren <41287460+SDoehren@users.noreply.github.com>
2024-11-05 22:38:40 +0000

    Init

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,11 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
     dice3d.addDicePreset({
         type: "d20",
         labels: [
-            '!', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-            'modules/sdtdice/faces/gwen/d20/20-face.webp'
+            'modules/fairwinterdice/faces/gwen/d20/1-face.webp', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+            'modules/fairwinterdice/faces/gwen/d20/20-face.webp'
         ],
-        bumpMaps: [, , , , , , , , , , , , , , , , , , ,
-            "modules/sdtdice/faces/gwen/d20/20-bump.webp"
+        bumpMaps: ["modules/fairwinterdice/faces/gwen/d20/1-bump.webp", , , , , , , , , , , , , , , , , , ,
+            "modules/fairwinterdice/faces/gwen/d20/20-bump.webp"
         ],
 
         system: "Gwen"
