9d6c7ef49b8a8f67ef0c91664805e1a1d57dac0c
SDoehren <41287460+SDoehren@users.noreply.github.com>
2024-11-05 22:35:42 +0000

    Init

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,17 @@
-console.log("----------------------------------------------------")
 Hooks.on('diceSoNiceReady', (dice3d) => {
 
-dice3d.addSystem({id:"SDT",name:"SDT"},false);
-dice3d.addDicePreset({
-    type:"d20",
-    labels:[
-   'modules/sdtdice/faces/oops.webp','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
-      'modules/sdtdice/faces/ranger-symbol.webp'
-    ],
-bumpMaps: ["modules/sdtdice/faces/oops-bump.webp",,,,,,,,,,,,,,,,,,,
-    "modules/sdtdice/faces/ranger-bump.webp"
-  ],
+    dice3d.addSystem({id: "Gwen", name: "Gwen"}, false);
+    dice3d.addDicePreset({
+        type: "d20",
+        labels: [
+            '!', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+            'modules/sdtdice/faces/gwen/d20/20-face.webp'
+        ],
+        bumpMaps: [, , , , , , , , , , , , , , , , , , ,
+            "modules/sdtdice/faces/gwen/d20/20-bump.webp"
+        ],
 
-    system:"SDT"
-  });
+        system: "Gwen"
+    });
 
 })
